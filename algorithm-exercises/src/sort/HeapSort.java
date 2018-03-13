355d3f33b182b37c43719f96793ab19be0cfb109
theozhang <theo.zhang@coupang.com>
2018-03-13 11:29:34 +0900

    Heap sort

--- algorithm-exercises/src/sort/HeapSort.java
+++ algorithm-exercises/src/sort/HeapSort.java
@@ -6,43 +6,60 @@ import sort.api.Sort;
 import java.util.Arrays;
 
 /**
+ * Min peak heap
+ *
  * @author theozhang on 3/12/18
  */
 public class HeapSort extends Sort implements HeapHandleable {
-
-    public int[] a = {1, 23, 54, 12, 54, 12};
+    //a[0] is used for filling index
+    public int[] a = {0,3, 2, 5, 2, 1, 7, 10, 9, 11, 8, 6};
 
     public static void main(String[] args) {
         HeapSort heapSort = new HeapSort();
         heapSort.buildHeap(heapSort.a);
+        heapSort.priorityPop(heapSort.a);
         System.out.println(Arrays.toString(heapSort.a));
     }
 
     public void buildHeap(int[] a) {
-        for (int i = a.length / 2; i >= 0; i--) {
+        for (int i = (a.length - 1) / 2; i > 0; i--) {
             sink(a, i, a.length - 1);
+        }
+    }
+
+    public void priorityPop(int[] a){
+        int end = a.length - 1;
+        while (end > 0){
+            exchange(a,1,end);
+            sink(a,1,--end);
         }
     }
 
     @Override
     public void sink(int[] a, int i, int j) {
-        while (2 * i <= j) {
-            if (2 * i == j) {
-                if (a[i] > a[2 * i]) {
-                    exchange(a, i, 2 * i);
-                    i = 2 * i;
-                }
-            } else if (2 * i + 1 <= j) {
-                if (a[2 * i] > a[2 * i + 1]) {
-                    exchange(a,i,2 * i + 1);
-                    i = 2 * i + 1;
-                } else {
-                    exchange(a,i,2 * i);
-                    i = 2 * i;
+        int left = 2 * i;
+        int right = 2 *i + 1;
+        while (left <= j) {
+            if (right > j){
+                if (a[left] < a[i]){
+                    exchange(a,left,i);
+                    i = left;
+                }else {
+                    break;
                 }
             }else {
-                break;
+                if (a[left] <= a[right] && a[i] > a[left]){
+                    exchange(a,left,i);
+                    i = left;
+                }else if (a[left] >= a[right] && a[i] > a[right]){
+                    exchange(a,right,i);
+                    i = right;
+                }else {
+                    break;
+                }
             }
+            left = 2 * i;
+            right = 2 * i + 1;
         }
     }
 
