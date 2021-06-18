e4fab6ce4f97dde6f37e40b2c3c32e90a5e167ab
TheoZhang <theo.zhang.tao@gmail.com>
2021-06-18 17:14:31 +0800

    LeetCode-实现indexOf

---
+++ algorithm-exercises/src/leetcode/T0028_StrStr.java
@@ -0,0 +1,61 @@
+package leetcode;
+
+/**
+ * 实现 strStr() 函数。
+ * <p>
+ * 给你两个字符串 haystack 和 needle ，请你在 haystack 字符串中找出 needle 字符串出现的第一个位置（下标从 0 开始）。如果不存在，则返回  -1 。
+ * <p>
+ *  
+ * <p>
+ * 说明：
+ * <p>
+ * 当 needle 是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * <p>
+ * 对于本题而言，当 needle 是空字符串时我们应当返回 0 。这与 C 语言的 strstr() 以及 Java 的 indexOf() 定义相符。
+ * <p>
+ * <p>
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/implement-strstr
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 张涛
+ * @date 6/18/21 5:05 PM
+ */
+public class T0028_StrStr {
+
+    public static void main(String[] args) {
+        T0028_StrStr t = new T0028_StrStr();
+        System.out.println(t.strStr("mississippi", "issip"));
+    }
+
+    public int strStr(String haystack, String needle) {
+        char[] hc = haystack.toCharArray();
+        char[] nc = needle.toCharArray();
+        if (nc.length == 0) {
+            return 0;
+        }
+        if (hc.length < nc.length) {
+            return -1;
+        }
+        int k;
+        for (int i = 0; i < hc.length - nc.length + 1; i++) {
+            if (hc[i] == nc[0]) {
+                k = 1;
+                boolean match = true;
+                while (k < nc.length) {
+                    if (nc[k] != hc[i + k]) {
+                        match = false;
+                        break;
+                    } else {
+                        k++;
+                    }
+                }
+                if (match) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
---
+++ algorithm-exercises/src/leetcode/T0029_Divide.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+/**
+ * @author 张涛
+ * @date 6/18/21 4:31 PM
+ */
+public class T0029_Divide {
+
+    public static void main(String[] args) {
+        T0029_Divide t = new T0029_Divide();
+        System.out.println(t.divide(-2147483648, -1));
+    }
+
+    public int divide(int dividend, int divisor) {
+        return 1;
+    }
+
+
+}
