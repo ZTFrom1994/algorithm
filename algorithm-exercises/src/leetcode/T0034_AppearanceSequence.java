d73ad316a42ef118ad6ef1c8f92f4cf73c2d2f0d
TheoZhang <theo.zhang.tao@gmail.com>
2021-06-28 10:14:30 +0800

    LeetCode-外观数列

---
+++ algorithm-exercises/src/leetcode/T0034_AppearanceSequence.java
@@ -0,0 +1,59 @@
+package leetcode;
+
+/**
+ * 给定一个正整数 n ，输出外观数列的第 n 项。
+ * <p>
+ * 「外观数列」是一个整数序列，从数字 1 开始，序列中的每一项都是对前一项的描述。
+ * <p>
+ * 你可以将其视作是由递归公式定义的数字字符串序列：
+ * <p>
+ * countAndSay(1) = "1"
+ * countAndSay(n) 是对 countAndSay(n-1) 的描述，然后转换成另一个数字字符串。
+ * 前五项如下：
+ * <p>
+ * 1.     1
+ * 2.     11
+ * 3.     21
+ * 4.     1211
+ * 5.     111221
+ * 第一项是数字 1
+ * 描述前一项，这个数是 1 即 “ 一 个 1 ”，记作 "11"
+ * 描述前一项，这个数是 11 即 “ 二 个 1 ” ，记作 "21"
+ * 描述前一项，这个数是 21 即 “ 一 个 2 + 一 个 1 ” ，记作 "1211"
+ * 描述前一项，这个数是 1211 即 “ 一 个 1 + 一 个 2 + 二 个 1 ” ，记作 "111221"
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/count-and-say
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author 张涛
+ * @date 6/28/21 9:33 AM
+ */
+public class T0034_AppearanceSequence {
+
+    public static void main(String[] args) {
+        T0034_AppearanceSequence t = new T0034_AppearanceSequence();
+        System.out.println(t.countAndSay(5));
+    }
+
+    public String countAndSay(int n) {
+        if (n == 1) {
+            return "1";
+        }
+        char[] chars = countAndSay(n - 1).toCharArray();
+        StringBuilder sb = new StringBuilder();
+        int count = 1;
+        for (int i = 0; i < chars.length - 1; i++) {
+            if (chars[i] == chars[i + 1]) {
+                count++;
+            } else {
+                sb.append(count);
+                sb.append(chars[i]);
+                count = 1;
+            }
+        }
+        sb.append(count);
+        sb.append(chars.length - 1);
+        return sb.toString();
+    }
+}
