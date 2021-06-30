8821e848acccc7a62b4242312abdfcc20e287100
TheoZhang <theo.zhang.tao@gmail.com>
2021-06-30 10:15:10 +0800

    选择排序和快排添加注释

--- algorithm-exercises/src/sort/QuickSort.java
+++ algorithm-exercises/src/sort/QuickSort.java
@@ -8,30 +8,52 @@ import java.util.Arrays;
  * @author theozhang on 3/12/18
  */
 public class QuickSort extends Sort {
-    public int[] a = {5,4,3,2,1};
+
+    public int[] a = {5, 4, 3, 2, 1};
+
     public static void main(String[] args) {
         QuickSort quickSort = new QuickSort();
-        quickSort.sort(quickSort.a,0, quickSort.a.length - 1);
+        quickSort.sort(quickSort.a, 0, quickSort.a.length - 1);
         System.out.println(Arrays.toString(quickSort.a));
     }
 
-    public int arrange(int[] a,int low, int high) {
-        int k = low;
+    /**
+     * 此函数返回一个k位置，所有小于k位置的值都会被放到k左边，所有大于k位置的都会放到k右边
+     *
+     * @param a
+     * @param low
+     * @param high
+     * @return
+     */
+    public int arrange(int[] a, int low, int high) {
+        int k = low;//目标元素位置，默认取范围内第一个即可，取其他的也行的
         while (low < high) {
-            while (low <= high && a[low] <= a[k]) low++;
-            while (high >= low && a[high] >= a[k]) high--;
-            if (low >= high) break;
-            swap(a,low, high);
+            //找到第一个大于k位置的值
+            while (low < high && a[low] < a[k]) low++;
+            //找到第一个小于k位置的值
+            while (low < high && a[high] > a[k]) high--;
+            swap(a, low, high);
         }
-        swap(a,k, high);
+        swap(a, k, high);
         return high;
     }
 
-    public void sort(int[] a,int low, int high) {
-        if (low >= high)
+    /**
+     * 快速排序核心思想：只要找到每一个元素在整个有序序列的位置，就可以完成排序
+     * arrange函数：找到low位置在low-high范围内的目标位置，返回这个位置k
+     * 实现方法：找到low位置在整个范围内排序后应该所处的位置，然后对K的左边和k的右边递归排序
+     *
+     * @param a
+     * @param low
+     * @param high
+     */
+    public void sort(int[] a, int low, int high) {
+        if (high - low <= 1) {
             return;
-        int k = arrange(a,low, high);
-        sort(a,low, k - 1);
-        sort(a,k + 1, high);
+        }
+        int k = arrange(a, low, high);
+        //对k左边排序
+        sort(a, low, k - 1);
+        sort(a, k + 1, high);
     }
 }
--- algorithm-exercises/src/sort/SelectionSort.java
+++ algorithm-exercises/src/sort/SelectionSort.java
@@ -8,23 +8,26 @@ import java.util.Arrays;
  * @author theozhang on 3/12/18
  */
 public class SelectionSort extends Sort {
-    public int a[] = {2,3,4,15,6,2};
-
     public static void main(String[] args) {
         SelectionSort selectionSort = new SelectionSort();
-        selectionSort.sort();
+        int[] a = {2, 3, 4, 15, 6, 2};
+        selectionSort.sort(a);
+        System.out.println(Arrays.toString(a));
     }
 
-    public  void sort() {
-        for (int i = 0;i < a.length;i++){
-            int minIndex = i;
-            for (int j = i + 1;j < a.length;j++){
-                if (a[j] < a[minIndex]){
-                    minIndex = j;
+    /**
+     * 选择排序：从未排序的列表中找出最小/大值，放在序列起始位置，然后再从未排序列表找出最小/大值，以此类推，得到排序后的序列
+     * 实现方法：按顺序遍历，对于每个元素i，找出其后面所有元素的最小/大值，然后和i换位置，当全部遍历完成后，则排序完成
+     */
+    public void sort(int[] a) {
+        for (int i = 0; i < a.length - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] < a[min]) {
+                    min = j;
                 }
             }
-            swap(a,i,minIndex);
+            swap(a, i, min);
         }
-        System.out.println(Arrays.toString(a));
     }
 }
