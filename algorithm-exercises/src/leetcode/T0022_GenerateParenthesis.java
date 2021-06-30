b36bca7e6e7bf657cfdb2dc00989d18fe40a1ed0
TheoZhang <theo.zhang.tao@gmail.com>
2021-06-30 14:59:34 +0800

    LeetCode-括号生成

--- algorithm-exercises/src/leetcode/T0022_GenerateParenthesis.java
+++ algorithm-exercises/src/leetcode/T0022_GenerateParenthesis.java
@@ -1,9 +1,14 @@
 package leetcode;
 
+import sort.api.Sort;
+
 import java.util.*;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.stream.Collectors;
 
 /**
  * 数字 n 代表生成括号的对数，请你设计一个函数，用于能够生成所有可能的并且 有效的 括号组合。
+ *
  * @author 张涛
  * @date 6/17/21 4:40 PM
  */
@@ -15,19 +20,25 @@ public class T0022_GenerateParenthesis {
     }
 
     public List<String> generateParenthesis(int n) {
-        if (n == 0) {
-            return new ArrayList<>();
+        List<String> ans = new ArrayList<String>();
+        backtrack(ans, new StringBuilder(), 0, 0, n);
+        return ans;
+    }
+
+    public void backtrack(List<String> ans, StringBuilder cur, int open, int close, int max) {
+        if (cur.length() == max * 2) {
+            ans.add(cur.toString());
+            return;
         }
-        if (n == 1) {
-            return Arrays.asList("()");
+        if (open < max) {
+            cur.append('(');
+            backtrack(ans, cur, open + 1, close, max);
+            cur.deleteCharAt(cur.length() - 1);
         }
-        List<String> list = generateParenthesis(n - 1);
-        Set<String> set = new HashSet<>();
-        for (String k : list) {
-            set.add("()" + k);
-            set.add(k + "()");
-            set.add("(" + k + ")");
+        if (close < open) {
+            cur.append(')');
+            backtrack(ans, cur, open, close + 1, max);
+            cur.deleteCharAt(cur.length() - 1);
         }
-        return new ArrayList<>(set);
     }
 }
